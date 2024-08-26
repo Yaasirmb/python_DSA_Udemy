e24827cf24e5342bbe7a3c7b77f8126406598df9
Yaasir Mahamadou-Bachirou <yaasir@Yaasirs-Laptop.attlocal.net>
2024-08-26 16:44:51 -0400

    Traverse array code.

---
+++ oneDimensionalArray.py
@@ -0,0 +1,13 @@
+# Code from the Array section of the course, going over 1D array practice.
+
+from array import *
+
+# 1. Create an array and traverse.
+# Int array
+myArray = array('i',[1,2,3,4,5])
+
+# O(n) time complexity
+for i in myArray:
+    print(i)
+
+    
